8166a37eafffb3dde51fd6b61bb383ee6ed92822
albemlee <78619678+AlbertLeeUCSF@users.noreply.github.com>
2022-09-08 10:01:23 -0700

    removed python

--- scripts/00_get_data.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from datasets import load_dataset
-
-if __name__ == '__main__':
-    jeopardy_df = load_dataset('jeopardy')
-    jeopardy_df['train'].to_csv('data/raw/jeopardy.csv', index = False)
-
